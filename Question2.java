28efc69190a275babec2afefe004d40663a3fda0
pineapplez23 <beyonddl17@gmail.com>
2022-04-24 14:22:53 +0800

    Did question 1 and 2 on VS code

--- Question2.java
+++ Question2.java
@@ -11,6 +11,10 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter your height(m): ");
     double height = in.nextDouble();
+    System.out.print("Enter your weight(kg): ");
+    double weight = in.nextDouble();
+    System.out.println("Your BMI is: " + (weight/(height*height)));
   }
 }
